a272b5bc331dae82c8d91a522dc3f1fbd0a0ac69
Ziaul Karim <ziaulkarim278@gmail.com>
2025-08-22 22:06:45 +0600

    Assign new list / in-place two pointer

--- Leetcode/922. Sort Array By Parity II.py
+++ Leetcode/922. Sort Array By Parity II.py
@@ -1,15 +1,29 @@
 class Solution:
     def sortArrayByParityII(self, nums):
         even, odd = 0, 1
-        new_nums = [-1] * len(nums)
-        for i in range(len(nums)):
-            if nums[i] % 2 and odd < len(nums):
-                new_nums[odd] = nums[i]
+
+        # Using new list
+        # new_nums = [-1] * len(nums)
+        # for i in range(len(nums)):
+        #     if nums[i] % 2:
+        #         new_nums[odd] = nums[i]
+        #         odd += 2
+        #     else:
+        #         new_nums[even] = nums[i]
+        #         even += 2
+        # return new_nums
+
+        # Using in-place
+        while(even < len(nums) and odd < len(nums)):
+            if nums[even] % 2 == 0:
+                even += 2
+            elif nums[odd] % 2 == 1:
                 odd += 2
             else:
-                new_nums[even] = nums[i]
+                nums[even], nums[odd] = nums[odd], nums[even]
                 even += 2
-        return new_nums
+                odd += 2
+        return nums
 
 nums = list(map(int, input().split()))
 obj = Solution()
