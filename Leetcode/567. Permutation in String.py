152f9f91ed15ed8917a4537a488484fd7d83ddf4
Ziaul Karim <ziaulkarim278@gmail.com>
2025-09-16 19:32:17 +0600

    2 diff. approaches

--- Leetcode/567. Permutation in String.py
+++ Leetcode/567. Permutation in String.py
@@ -2,13 +2,21 @@ class Solution:
     def checkInclusion(self, s1, s2):
 
         # Approach 1
+        # n, m = len(s2), len(s1)
+        # i = 0
+        # while i < n:
+        #     if i + m <= n and sorted(s1) == sorted(s2[i:i + m]):
+        #         return True
+        #     i += 1
+        # return False
+
+        # Approach 2
         n, m = len(s2), len(s1)
-        i = 0
-        while i < n:
-            if i + m <= n and sorted(s1) == sorted(s2[i:i + m]):
+        for i in range(n - m + 1):
+            if sorted(s1) == sorted(s2[i:i + m]):
                 return True
-            i += 1
         return False
+
 
 
 s1 = input()
