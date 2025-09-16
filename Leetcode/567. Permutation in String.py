7535e76a81c08800734da7804ad454d3bbf48665
Ziaul Karim <ziaulkarim278@gmail.com>
2025-09-16 18:08:09 +0600

    String maching

---
+++ Leetcode/567. Permutation in String.py
@@ -0,0 +1,17 @@
+class Solution:
+    def checkInclusion(self, s1, s2):
+
+        # Approach 1
+        n, m = len(s2), len(s1)
+        i = 0
+        while i < n:
+            if i + m <= n and sorted(s1) == sorted(s2[i:i + m]):
+                return True
+            i += 1
+        return False
+
+
+s1 = input()
+s2 = input()
+obj = Solution()
+print(obj.checkInclusion(s1, s2))
