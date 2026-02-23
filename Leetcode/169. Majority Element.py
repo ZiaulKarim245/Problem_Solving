2562a7ec8ebde4c12948ac71b1c9972c3e520a67
Ziaul Karim <ziaulkarim278@gmail.com>
2026-02-23 11:43:01 +0600

    Sort and find majority element

--- Leetcode/169. Majority Element.py
+++ Leetcode/169. Majority Element.py
@@ -1,12 +1,14 @@
 class Solution:
     def majorityElement(self, nums: list[int]) -> int:
+        # Approach 1: Sorting
         n, maj_element, cnt = len(nums), 0, 1
         nums.sort()
-        for i in range(1, n):
-            if nums[i] != nums[i - 1] and cnt > n//2:
-                maj_element = nums[i - 1]
-                cnt = 0
-            cnt += 1
-        if cnt > n//2:
-            maj_element = nums[n - 1]
-        return maj_element
+        # for i in range(1, n):
+        #     if nums[i] != nums[i - 1] and cnt > n//2:
+        #         maj_element = nums[i - 1]
+        #         cnt = 0
+        #     cnt += 1
+        # if cnt > n//2:
+        #     maj_element = nums[n - 1]
+        # return maj_element
+        return nums[n//2]
